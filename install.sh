12eb5f7aadbc57ea1de75897c28b36b6662b239e
Herbie <hjrand1005@gmail.com>
2022-08-08 22:30:25 -0400

    FIX: remove deprecated script, fix separator string

--- install.sh
+++ install.sh
@@ -4,23 +4,23 @@ readonly SEPARATOR="----------\n"
 echo "Update and Upgrade..."
 apt update
 apt upgrade 
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing vim..."
 apt-get -y install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Install neovim..."
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing git..."
 apt-get -y install git
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing vscode..."
 apt-get -y install wget gpg
@@ -31,7 +31,7 @@ rm -f packages.microsoft.gpg
 apt-get -y install apt-transport-https
 apt update
 apt-get -y install code
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing go version 1.19..."
 wget -c https://golang.org/dl/go1.19.linux-amd64.tar.gz
@@ -39,22 +39,22 @@ chmod +x go1.19.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin">>$HOME/.profile
 rm go1.19.linux-amd64.tar.gz
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing build-essential"
 apt install --reinstall build-essential
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing i3"
 apt-get -y install i3
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing tmux"
 apt-get -y install tmux
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Installing feh"
 apt install feh -y
-echo SEPARATOR
+echo $SEPARATOR
 
 echo "Finished"

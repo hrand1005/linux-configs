c933060be4105b8d73456720118051fed8a1a682
Herbie <hjrand1005@gmail.com>
2022-08-13 12:28:39 -0400

    FIX: remove nvim execution, fix bash path

--- install.sh
+++ install.sh
@@ -15,7 +15,6 @@ echo $SEPARATOR
 echo "Install neovim..."
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
 echo $SEPARATOR
 
 echo "Installing git..."
--- setup_i3.sh
+++ setup_i3.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 cp configs/i3config $HOME/.config/i3/config
